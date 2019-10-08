3107fdede97735f0ec4372190a1c8e3f8ea6eae9
Stewart Clay <stewartclay166@gmail.com>
2019-10-08 17:48:47 +0200

    Figured out Sending data to View from model

--- Troye Computer Systems/Troye Computer Systems/Controllers/ColleaguesController.cs
+++ Troye Computer Systems/Troye Computer Systems/Controllers/ColleaguesController.cs
@@ -3,15 +3,39 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Troye_Computer_Systems.Models;
 
 namespace Troye_Computer_Systems.Controllers
 {
     public class ColleaguesController : Controller
     {
+        //public Colleagues g = new Colleagues();
         // GET: Colleagues
+        //[RequireHttps]
+        [HttpGet]
         public ActionResult Index()
         {
-            return View("CollaguesView");
+            //Colleagues col = new Colleagues
+            ////{
+            ////    FirstName = "Philip"
+            ////};
+            ////ViewBag.Message = col;
+            ///
+            //var model = Troye_Computer_Systems.Models.Colleagues.FirstName;
+            //return View("ColleaguesView", model);
+
+            Colleagues g = new Colleagues();
+            
+
+            return View("ColleaguesView", g);
+
         }
+
+        //[HttpPost]
+        //public ActionResult getEmployeeDetails()
+        //{
+        //    return null;
+        //}
+
     }
 }
---
+++ Troye Computer Systems/Troye Computer Systems/Models/Colleagues.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Troye_Computer_Systems.Models
+{
+
+
+    public class Colleagues
+    {
+        //public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Skill { get; set; }
+        //public string Email { get; set; }
+        public  List<string> Eamil = new List<string>(new string[] { "@Philip", "@Stewart" });
+        public List<string> FirstName = new List<string>(new string[] {"Philip" , "Stewart" });
+
+
+        //public void S()
+        //{
+        //    FirstName.Add("s");
+        //}
+
+
+
+    }
+   
+}

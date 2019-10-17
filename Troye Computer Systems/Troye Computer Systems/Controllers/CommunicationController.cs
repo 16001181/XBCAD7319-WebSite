4d5bb7e05e34ecead7bf191eba5fdb175bc23dd9
Donovan <Donovan@DESKTOP-01VP4ET>
2019-10-17 10:33:18 +0200

    Communication View Added

---
+++ Troye Computer Systems/Troye Computer Systems/Controllers/CommunicationController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Troye_Computer_Systems.Controllers
+{
+    public class CommunicationController : Controller
+    {
+        // GET: Communication
+        public ActionResult Index()
+        {
+            return View("CommunicationView");
+        }
+    }
+}

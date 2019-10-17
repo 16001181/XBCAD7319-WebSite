a9cdc4c2fe115d0500875000ce392c08940cb258
Donovan <Donovan@DESKTOP-01VP4ET>
2019-10-17 10:57:42 +0200

    Commit ChangesComm

--- Troye Computer Systems/Troye Computer Systems/Controllers/CommunicationController.cs
+++ Troye Computer Systems/Troye Computer Systems/Controllers/CommunicationController.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-
+//This is where i will pull from firebase and store all messages
 namespace Troye_Computer_Systems.Controllers
 {
     public class CommunicationController : Controller
@@ -11,7 +11,22 @@ namespace Troye_Computer_Systems.Controllers
         // GET: Communication
         public ActionResult Index()
         {
+
             return View("CommunicationView");
+
+        }
+        [HttpPost]
+        public ActionResult postusingparameters(string name, string emailAddress, string subject, string body)
+        {
+            Models.Communication s = new Models.Communication();
+            s.Name = name;
+            s.EmailAddress = emailAddress;
+            s.Subject = subject;
+            s.GetMessage = body;
+            //add error checking
+            //return Content("<script language='javascript' type='text/javascript'>alert ('Email successfully sent I will be in contact with you as soon as possible! ');</script>");
+
+            return RedirectToAction("Index", "Home");
         }
     }
 }
---
+++ Troye Computer Systems/Troye Computer Systems/Models/Communication.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Troye_Computer_Systems.Models
+{
+    public class Communication
+    {
+        public string Name { get; set; }
+        public string EmailAddress { get; set; }
+        public string Subject { get; set; }
+        public string GetMessage { get; set; }
+            
+    }
+}

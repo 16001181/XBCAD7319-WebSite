b78932e8a43a3556bb718609216f7b37159c81d6
Donovan <Donovan@DESKTOP-01VP4ET>
2019-10-21 13:38:32 +0200

    Model Copy

--- Troye Computer Systems/Troye Computer Systems/Models/Communication.cs
+++ Troye Computer Systems/Troye Computer Systems/Models/Communication.cs
@@ -7,10 +7,14 @@ namespace Troye_Computer_Systems.Models
 {
     public class Communication
     {
-        public string Name { get; set; }
-        public string EmailAddress { get; set; }
-        public string Subject { get; set; }
-        public string GetMessage { get; set; }
-            
+        public int EmployeeNumber { get; set; }
+        public int cnt { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string CellNumber { get; set; }
+        public string Skill { get; set; }
+        public string[] TotalPurchasesLastThreeDays { get; set; }
+
     }
 }

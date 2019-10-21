56f3343b311e7bb6b355cf92b8264052624475eb
Donovan <Donovan@DESKTOP-01VP4ET>
2019-10-21 13:49:29 +0200

    Stewart Fix

--- Troye Computer Systems/Troye Computer Systems/Controllers/CommunicationController.cs
+++ Troye Computer Systems/Troye Computer Systems/Controllers/CommunicationController.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using FireSharp.Config;
+using FireSharp.Interfaces;
+using FireSharp.Response;
+using System;
 using System.Collections.Generic;
 using System.Data;
 using System.Linq;

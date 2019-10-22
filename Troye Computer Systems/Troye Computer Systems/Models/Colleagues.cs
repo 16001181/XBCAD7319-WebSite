9786bad4b065bd5c362b330b52bb83942b794c7a
16001181 <16001181@vcconnect.co.za>
2019-10-22 14:32:56 +0200

    Uploading to master for Colleagues view

---
+++ Troye Computer Systems/Troye Computer Systems/Models/Colleagues.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using FireSharp.Config;
+using FireSharp.Interfaces;
+using FireSharp.Response;
+namespace Troye_Computer_Systems.Models
+{
+
+
+    public class Colleagues
+    {
+        public int EmployeeNumber { get; set; }
+        public int cnt { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string CellNumber { get; set; }
+        public string Skill { get; set; }
+        public string[] TotalPurchasesLastThreeDays { get; set; }
+    }
+
+}
